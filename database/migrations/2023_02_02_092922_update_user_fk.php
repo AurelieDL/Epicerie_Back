4c5e0964e90289547b30fb8566257b74b9d48fd3
AurelieDL <91252299+AurelieDL@users.noreply.github.com>
2023-02-02 11:01:05 +0100

    BDD tables + relations products

--- database/migrations/2023_02_02_092922_update_user_fk.php
+++ database/migrations/2023_02_02_092922_update_user_fk.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->integer('role_id');
             $table->foreign('role_id')->references('id')->on('roles');
+            $table->integer('detail_id');
+            $table->foreign('detail_id')->references('id')->on('details');
         });
     }
 
@@ -29,6 +31,8 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->dropForeign(['role_id']);
             $table->dropColumn(['role_id']);
+            $table->dropForeign(['detail_id']);
+            $table->dropColumn(['detail_id']);
         });
     }
 };
