113d5652c15186106a9257d166116f0a53b515ee
AurelieDL <91252299+AurelieDL@users.noreply.github.com>
2023-02-03 16:06:44 +0100

    v-data table products (route)

---
+++ app/Http/Controllers/api/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::with('status', 'supplier', 'creator')->get();
+        return $products;
+    }
+}
--- app/Http/Controllers/api/RoleController.php
+++ app/Http/Controllers/api/RoleController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\api;
 
 use App\Models\Role;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class RoleController extends Controller
 {
