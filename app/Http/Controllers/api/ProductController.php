3c2f64d74b2fddde9b89d39402e0a48603ad23c4
AurelieDL <91252299+AurelieDL@users.noreply.github.com>
2023-03-30 17:58:22 +0200

    article test

--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\api;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Product;
+use Illuminate\Support\Facades\Log;
 
 class ProductController extends Controller
 {
@@ -17,5 +18,66 @@ class ProductController extends Controller
     {
         $products = Product::with('status', 'supplier', 'creator')->get();
         return $products;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $userConnected = auth()->user();
+        Log::info('user connected produit');
+        Log::info(json_encode($userConnected));
+        $newProduct = new Product();
+        $newProduct->name = $request->name;
+        $newProduct->quantity = $request->quantity;
+        $newProduct->packaging = $request->packaging;
+        $newProduct->price_ht = $request->price_ht;
+        $newProduct->tva = $request->vat;
+        $newProduct->margin_rate = $request->margin_rate;
+        $newProduct->price_ttc = $request->price_ttc;
+        $newProduct->supplier_id = $request->supplier;
+        $newProduct->created_by = 1;
+        $newProduct->status_id = 1;
+
+        $newProduct->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //
     }
 }
--- app/Http/Controllers/api/authController.php
+++ app/Http/Controllers/api/authController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers\api;
+
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 
@@ -15,7 +17,13 @@ class authController extends Controller
     {
         $user = User::where('email', $request->email)->first();
 
-        if(Hash::check($request->password, $user->password)) {
+
+        if (Hash::check($request->password, $user->password)) {
+            Auth::attempt(['email' =>  $request->email, 'password' => $request->password]);
+            $userConnected = auth()->user();
+            Log::info('user connected log route');
+            Log::info(json_encode($userConnected));
+
             return response()->json([
                 'token' => $user->createToken(time())->plainTextToken
             ]);
